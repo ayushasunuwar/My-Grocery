e178b0b80f1561c6542ecab39c0fc8ed89943432
ayushasunuwar <aisharodriguez91@gmail.com>
2026-01-06 12:31:40 +0545

    Initial commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import Hero from "./components/Hero/Hero"
+import  Home  from "./components/Home/Home"
+
+
+function App() {
+
+  return (
+    <>
+      <Home/>
+      <Hero/>
+    </>
+  )
+}
+
+export default App
---
+++ Frontend/src/components/Hero/Hero.jsx
@@ -0,0 +1,19 @@
+
+
+const Hero = () => {
+  return (
+    <section>
+        <div className="max-w-[1400px] mx-auto px-10">
+            {/* hero content */}
+            <div></div>
+
+            {/* Hero Image */}
+            <div>
+                <img src="" alt="" />
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Hero
