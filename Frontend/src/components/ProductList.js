775c80a32046713ed6f5d01c0caba79c681db02a
ayushasunuwar <aisharodriguez91@gmail.com>
2026-01-08 17:57:09 +0545

    Products display completed

---
+++ Frontend/src/components/ProductList.js
@@ -0,0 +1,180 @@
+import Banana from "../assets/banana.png"
+import Beef from "../assets/beef.png"
+import Brocolli from "../assets/broccoli.png"
+import Butter from "../assets/butter.png"
+import Cabbage from "../assets/cabbage.png"
+import Capsicum from "../assets/capsicum.png"
+import Cheese from "../assets/cheese.png"
+import ChickenBreast from "../assets/chicken-breast.png"
+import CondensedMilk from "../assets/condensed-milk.png"
+import Eggplant from "../assets/eggplant.png"
+import Eggs from "../assets/eggs.png"
+import Grapes from "../assets/grapes.png"
+import Kale from "../assets/kale.png"
+import Kiwi from "../assets/kiwi.png"
+import Lettuce from "../assets/lettuce.png"
+import Milk from '../assets/milk.png'
+import Pineapple from "../assets/pineapple.png"
+import RicottaCheese from "../assets/ricotta-cheese.png"
+import Salmon from "../assets/salmon.png"
+import Shrimp from "../assets/shrimp.png"
+import SliceCheese from "../assets/slice-cheese.png"
+import Strawberry from "../assets/strawberry.png"
+import Tilapia from "../assets/tilapia.png"
+import Tofu from "../assets/tofu.png"
+import Yogurt from "../assets/yogurt.png"
+
+const ProductList = [
+    {
+        id: 1,
+        name: 'Banana',
+        price: 20,
+        image: Banana
+    },
+    {
+        id: 2,
+        name: 'Beef',
+        price: 20,
+        image: Beef
+    },
+    {
+        id: 3,
+        name: 'Brocolli',
+        price: 20,
+        image: Brocolli
+    },
+    {
+        id: 4,
+        name: 'Butter',
+        price: 20,
+        image: Butter
+    },
+    {
+        id: 5,
+        name: 'Cabbage',
+        price: 20,
+        image: Cabbage
+    },
+    {
+        id: 6,
+        name: 'Capsicum',
+        price: 20,
+        image: Capsicum
+    },
+    {
+        id: 7,
+        name: 'Cheese',
+        price: 20,
+        image: Cheese
+    },
+    {
+        id: 8,
+        name: 'ChickenBreast',
+        price: 20,
+        image: ChickenBreast
+    },
+    {
+        id: 9,
+        name: 'CondensedMilk',
+        price: 20,
+        image: CondensedMilk
+    },
+    {
+        id: 10,
+        name: 'Eggplant',
+        price: 20,
+        image: Eggplant
+    },
+    {
+        id: 11,
+        name: 'Eggs',
+        price: 20,
+        image: Eggs
+    },
+    {
+        id: 12,
+        name: 'Grapes',
+        price: 20,
+        image: Grapes
+    },
+    {
+        id: 13,
+        name: 'Kale',
+        price: 20,
+        image: Kale
+    },
+    {
+        id: 14,
+        name: 'Kiwi',
+        price: 20,
+        image: Kiwi
+    },
+    {
+        id: 15,
+        name: 'Lettuce',
+        price: 20,
+        image: Lettuce
+    },
+    {
+        id: 16,
+        name: 'Milk',
+        price: 20,
+        image: Milk
+    },
+    {
+        id: 17,
+        name: 'Pineapple',
+        price: 20,
+        image: Pineapple
+    },
+    {
+        id: 18,
+        name: 'RicottaCheese',
+        price: 20,
+        image: RicottaCheese
+    },
+    {
+        id: 19,
+        name: 'Salmon',
+        price: 20,
+        image: Salmon
+    },
+    {
+        id: 20,
+        name: 'Shrimp',
+        price: 20,
+        image: Shrimp
+    },
+    {
+        id: 21,
+        name: 'SliceCheese',
+        price: 20,
+        image: SliceCheese
+    },
+    {
+        id: 22,
+        name: 'Strawberry',
+        price: 20,
+        image: Strawberry
+    },
+    {
+        id: 23,
+        name: 'Tilapia',
+        price: 20,
+        image: Tilapia
+    },
+    {
+        id: 24,
+        name: 'Tofu',
+        price: 20,
+        image: Tofu
+    },
+    {
+        id: 25,
+        name: 'Yogurt',
+        price: 20,
+        image: Yogurt
+    },
+];
+
+export default ProductList;
--- Frontend/src/components/Products/Products.jsx
+++ Frontend/src/components/Products/Products.jsx
@@ -1,9 +1,18 @@
-import Button from "../Button/Button";
+import { useState } from "react";
 import Heading from "../Heading/Heading"
-
+import ProductList from "../ProductList";
+import Cards from "../Cards/Cards";
+import Button from "../Button/Button";
 
 const Products = () => {
   const categories = ['All', 'Fruits', 'Vegetables', 'Dairy', 'Seafood'];
+  const [activeTab, setActiveTab] = useState('All');
+
+  const renderCards = ProductList.slice(0, 8).map(product => {
+    return(
+      <Cards image={product.image} name={product.name} price={product.price}/>
+    )
+  })
 
   return (
     <section>
@@ -11,13 +20,27 @@ const Products = () => {
             <Heading highlight="Our" heading="Products"/>
 
             {/* Tabs */}
-            <div>
-              {categories.map(categor => {
+            <div className="flex gap-3 justify-center mt-10">
+              {categories.map(category => {
                 return (
-                  <Button/>
+                  <button key={category} className={`px-5 py-2 text-lg cursor-pointer
+                  ${activeTab === category ? "bg-gradient-to-b from-orange-400 to-orange-500 text-white" : "bg-zinc-100"}`}
+                  onClick={() => setActiveTab(category)}>
+                    {category}
+                  </button>
                 )
               })}
             </div>
+
+            {/* Product Listing */}
+              <div className="grid grid-cols-4 gap-9 mt-20">
+                {renderCards}
+              </div>
+
+              <div className="mt-15 mx-auto w-fit">
+                <Button content="View All"/>
+              </div>
+
         </div>
     </section>
   )
